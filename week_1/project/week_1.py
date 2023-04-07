14d4b1d0fea97f1cf2a95e61fc179e11b3e76cd2
phillandin <phil.landin@gmail.com>
2023-04-07 15:22:25 +0000

    revise process_data_op to make only one Aggregation object

--- week_1/project/week_1.py
+++ week_1/project/week_1.py
@@ -12,7 +12,8 @@ from dagster import (
     op,
     usable_as_dagster_type,
     DynamicOut,
-    DynamicOutput
+    DynamicOutput,
+    Output
 )
 from pydantic import BaseModel
 
@@ -59,12 +60,12 @@ def get_s3_data_op(context):
     return stocks
 
 
-@op(ins={'stock_data': In(dagster_type=List, description='list of Stock objects')},
+@op(ins={'stock_data': In(dagster_type=List, description='list of stock data')},
     out={'high_date': Out(dagster_type=Aggregation, description='date with greatest high value')})
 def process_data_op(stock_data):
-    highs = map(lambda x: Aggregation(date=x.date, high=x.high), stock_data)
-    high_date = max(highs, key=lambda x: x.high)
-    return high_date
+    # highs = map(lambda x: Aggregation(date=x.date, high=x.high), stock_data)
+    high_date = max(stock_data, key=lambda x: x.high)
+    return Aggregation(date=high_date.date, high=high_date.high)
 
 
 @op(ins={'high_date': In(dagster_type=Aggregation)})
