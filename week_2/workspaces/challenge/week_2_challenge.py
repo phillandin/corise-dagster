361fbff98c2f1c4d69e207ad26df714cb942f932
phillandin <phil.landin@gmail.com>
2023-04-25 20:09:17 +0000

    rewrite with Dennis's solution

--- week_2/workspaces/challenge/week_2_challenge.py
+++ week_2/workspaces/challenge/week_2_challenge.py
@@ -49,9 +49,52 @@ def insert_dbt_data(context: OpExecutionContext, table_name: String):
     context.log.info("Batch inserted")
 
 
+@op(
+    required_resource_keys={"dbt"},
+    ins={"start_after": In(Nothing)},
+    out={
+        "success": Out(is_required=False),
+        "failure": Out(is_required=False),
+    },
+    tags={"kind": "dbt"},
+)
+def dbt_test_op_conditional(context: OpExecutionContext):
+    result = context.resources.dbt.test()
+    if result.return_code == 0:
+        yield Output(None, "success")
+    else:
+        yield Output(None, "failure")
+
+
+@op(
+    ins={"start_after": In(Nothing)},
+    out=Out(Nothing),
+)
+def success_dbt_op(context: OpExecutionContext):
+    context.log.info("Successful dbt run!")
+
+
+@op(
+    ins={"start_after": In(Nothing)},
+    out=Out(Nothing),
+)
+def failure_dbt_op(context: OpExecutionContext):
+    context.log.info("Issue with the dbt run!")
+
+
 @graph
 def dbt_graph():
-    pass
+    table = create_dbt_table()
+    inserted_data = insert_dbt_data(table)
+    dbt_run = dbt_run_op(inserted_data)
+    success, failure = dbt_test_op_conditional(dbt_run)
+    success_dbt_op(success)
+    failure_dbt_op(failure)
+
+
+local = {
+    "ops": {"create_dbt_table": {"config": {"table_name": ANALYTICS_TABLE}}},
+}
 
 
 docker = {
@@ -62,6 +105,22 @@ docker = {
     "ops": {"create_dbt_table": {"config": {"table_name": ANALYTICS_TABLE}}},
 }
 
+# This actually won't run with the ResourceDefinition.mock_resource() for dbt because the output will not meet the contract 
+dbt_job_local = dbt_graph.to_job(
+    name="week_2_challenge_local",
+    config=docker,
+    resource_defs={
+        "database": ResourceDefinition.mock_resource(),
+        "dbt": ResourceDefinition.mock_resource(),
+    },
+)
+
+
 dbt_job_docker = dbt_graph.to_job(
     name="week_2_challenge_docker",
+    config=docker,
+    resource_defs={
+        "database": postgres_resource,
+        "dbt": dbt_cli_resource,
+    },
 )
